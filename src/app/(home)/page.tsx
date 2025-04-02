bb785a13afaf208f38056a829b5951a4834e9e44
ciza Wells <cizawells70@gmail.com>
2025-04-02 14:58:53 +0200

    adding clerk and at 1:37:48

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      <Image src="/logo.svg" height={30} width={30} alt="Logo"/>
-      <p className="text-xl font-semibold tracking-tight">NewTube</p>
-    </div>
-  );
+  return <div>I will load videos in the future</div>;
 }
