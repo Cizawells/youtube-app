37c2930832a452025bdb88b59cbcf717f7215615
Cizawells <cizawells70@gmail.com>
2025-04-07 08:27:18 +0200

    working on trpc up to 3:40:51

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div>I will load videos in the future</div>;
+import { HydrateClient, trpc } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  const data = await trpc.hello({
+    text: "Wells",
+  });
+  return (
+    <div>
+      <HydrateClient>
+        <Suspense fallback={<p>Loading...</p>}>
+          <ErrorBoundary fallback={<p>Error</p>}>Client</ErrorBoundary>
+        </Suspense>
+      </HydrateClient>
+    </div>
+  );
 }
