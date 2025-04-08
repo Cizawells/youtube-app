2e86192ec7e97d540933c165fac0f9d542c5a46f
Cizawells <cizawells70@gmail.com>
2025-04-09 00:12:32 +0200

    applying categories schema changes

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { unique } from "drizzle-orm/gel-core";
 import {
   pgTable,
   text,
@@ -18,4 +19,17 @@ export const users = pgTable(
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
   },
   (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
+);
+
+export const categories = pgTable(
+  "categories",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull().unique(),
+    description: text("description"),
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("name_idx").on(t.name)]
 );
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -5,6 +5,8 @@ import { initTRPC, TRPCError } from "@trpc/server";
 import { eq } from "drizzle-orm";
 import { cache } from "react";
 import superjson from "superjson";
+import { Redis } from "@upstash/redis";
+import { rateLimit } from "@/lib/rateLimit";
 
 export const createTRPCContext = cache(async () => {
   const { userId } = await auth();
@@ -27,19 +29,26 @@ const t = initTRPC.context<Context>().create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
 export const protectedProcedure = t.procedure.use(
   async function isAuthed(opts) {
     const { ctx } = opts;
     if (!ctx.clerkUserId) {
       throw new TRPCError({ code: "UNAUTHORIZED" });
     }
+
     const [user] = await db
       .select()
       .from(users)
-      .where(eq(users.id, ctx.clerkUserId));
+      .where(eq(users.clerkId, ctx.clerkUserId));
 
     if (!user) {
       throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+    const { success } = await rateLimit.limit(user.id);
+
+    if (!success) {
+      throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
     }
 
     return opts.next({
