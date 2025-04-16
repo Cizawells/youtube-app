f2da9af62728ec6db61948f9ec7f1fd44924f790
ciza Wells <cizawells70@gmail.com>
2025-04-16 17:28:19 +0200

    worked up to 3:40:55

--- src/filter-carousel.tsx
+++ src/filter-carousel.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Badge } from "./components/ui/badge";
 import {
   Carousel,
+  CarouselApi,
   CarouselContent,
   CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
 } from "./components/ui/carousel";
+import { Skeleton } from "./components/ui/skeleton";
+import { cn } from "./lib/utils";
 
 interface FilterCarouselProps {
   value?: string | null;
@@ -23,25 +29,78 @@ export const FilterCarousel = ({
   data,
   isLoading,
 }: FilterCarouselProps) => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  });
   return (
-    <div className="relative w-fufll">
+    <div className="relative w-full">
+      {/* Left fade */}
+      <div
+        className={cn(
+          "absolute left-12 top-0 bottom-0 w-12 z-10 bg-gradient-to-r from-white to-transparent pointer-events-none",
+          current === 1 && "hidden"
+        )}
+      />
       <Carousel
+        setApi={setApi}
         opts={{
-          align: "start",
+          align: "center",
           dragFree: true,
         }}
+        className="w-3/6 m-auto bg-black"
       >
         <CarouselContent className="-ml-3">
-          <CarouselItem>
-            <Badge
-              variant={value === null ? "default" : "secondary"}
-              className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
-            >
-              All
-            </Badge>
+          <CarouselItem className="pl-3 basis-auto">
+            {!isLoading && (
+              <Badge
+                variant={!value ? "default" : "secondary"}
+                className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
+              >
+                All
+              </Badge>
+            )}
           </CarouselItem>
+          {isLoading &&
+            Array.from({ length: 14 }).map((_, index) => (
+              <CarouselItem key={index} className="pl-3 basis-auto">
+                <Skeleton className="rounded-lg px-3 py-1 h-full text-sm w-[100px] font-semibold" />
+                &nbsp;
+              </CarouselItem>
+            ))}
+          {!isLoading &&
+            data.map((item) => (
+              <CarouselItem key={item.value} className="pl-3 basis-auto">
+                <Badge
+                  variant={value === item.value ? "default" : "secondary"}
+                  className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
+                >
+                  {item.label}
+                </Badge>
+              </CarouselItem>
+            ))}
         </CarouselContent>
+        <CarouselPrevious className="left-2 -translate-x-full z-20" />
+        <CarouselNext className="right-2 translate-x-full z-20" />
       </Carousel>
+
+      {/* Left fade */}
+      <div
+        className={cn(
+          "absolute right-12 top-0 bottom-0 w-12 z-10 bg-gradient-to-l from-white to-transparent pointer-events-none",
+          current === count && "hidden"
+        )}
+      />
     </div>
   );
 };
