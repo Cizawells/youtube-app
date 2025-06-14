ca13d89ce2d08e13331f4f38fae45e7a619854c7
Cizawells <cizawells70@gmail.com>
2025-06-14 07:35:31 +0200

    worked on mux webhook

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -1,5 +1,8 @@
+import { db } from "@/db";
+import { videos } from "@/db/schema";
 import { mux } from "@/lib/mux";
 import { VideoAssetCreatedWebhookEvent, VideoAssetErroredWebhookEvent, VideoAssetReadyWebhookEvent, VideoAssetTrackReadyWebhookEvent } from "@mux/mux-node/resources/webhooks.mjs"
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 
 const SIGNING_SECRET = process.env.MUX_WEBHOOK_SECRET
@@ -36,6 +39,20 @@ type WebhookEvent =
         switch(payload.type as WebhookEvent["type"]) {
             case "video.asset.created":
                 const data = payload.data as VideoAssetCreatedWebhookEvent["data"];
-                
+
+                if(!data.upload_id) {
+                    return new Response("No upload ID found", { status: 400})
+                }
+
+                await db.update(videos)
+                    .set({
+                        muxAssetId: data.id,
+                        muxStatus: data.status,
+                    })
+                    .where(eq(videos.muxUploadId, data.upload_id));
+                    break;
+
         }
+
+        return new Response("Webhook received", { status: 200})
     }
